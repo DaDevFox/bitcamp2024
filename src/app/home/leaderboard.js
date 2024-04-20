e72b288f3ddbcec6846e233422d04944d04fdfe4
Mehul Tahiliani <mehul.tahiliani@gmail.com>
2024-04-20 14:03:58 -0400

    fix merge conflict + package problem

--- src/app/home/leaderboard.js
+++ src/app/home/leaderboard.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 import styles from "../page.module.css";
 
 export default function Leaderboard(props) {
   return <div className={styles.leaderboard}>{props.children}</div>;
 }
-=======
-export default function Leaderboard(props) {
-    
-}
-
->>>>>>> master
