083ca82f867748c39aa1c30b1d89e26f1554e1ca
Mehul Tahiliani <mehul.tahiliani@gmail.com>
2024-04-20 10:34:43 -0400

    dynamic podium component

--- src/app/home/podium.js
+++ src/app/home/podium.js
@@ -1,37 +1,16 @@
 import React from "react";
 import styles from "../page.module.css";
-
-export default function Podium({
-  base_height,
-  decrease_per_height,
-  ...titles
-}) {
-  var podiums;
-  var podium_count = 3;
-
-  for (var i = 0; i < podium_count; i++) {
-    podiums += (
-      <div className={styles.podium_item_container}>
-        <p className={styles.podium_item_text}>{titles[i]}</p>
-        <div className={styles.podium_item_fill}></div>
-      </div>
-    );
-  }
-
+export default function Podium(props) {
   return (
     <div className={styles.podium_container}>
-      <div className={styles.podium_item_container}>
-        <p className={styles.podium_item_text}>{props.title1 ?? 1}</p>
-        <div className={styles.podium_item_fill}></div>
-      </div>
-      <div className={styles.podium_item_container}>
-        <p className={styles.podium_item_text}>{props.title2 ?? 2}</p>
-        <div className={styles.podium_item_fill}></div>
-      </div>
-      <div className={styles.podium_item_container}>
-        <p className={styles.podium_item_text}>{props.title3 ?? 3}</p>
-        <div className={styles.podium_item_fill}></div>
-      </div>
+      {props.titles.map((title, index) => {
+        return (
+          <div key={index} className={styles.podium_item_container}>
+            <p className={styles.podium_item_text}>{title}</p>
+            <div className={styles.podium_item_fill}></div>
+          </div>
+        );
+      })}
     </div>
   );
 }
