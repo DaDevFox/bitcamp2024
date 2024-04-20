9b44fffd7809bebd6c319fcafa4d3ed9ef6672f8
Mehul Tahiliani <mehul.tahiliani@gmail.com>
2024-04-20 11:42:23 -0400

    fix main closing tag

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -10,45 +10,41 @@ export default function Home(props) {
   var titles = ["Easton", "Denton", "Elicott", "Hello"];
 
   const [scanResult, setScanResult] = useState(null);
-  
+
   useEffect(() => {
-    const scanner = new Html5QrcodeScanner('reader', {
+    const scanner = new Html5QrcodeScanner("reader", {
       qrbox: {
         width: 250, // height and width of the scanning box
-        height: 250
+        height: 250,
       },
       fps: 5, // fps for qr code scanning, don't make it too high
-      })
-  
-      scanner.render(success, error);
-  
-      function success(result){
-        scanner.clear();
-        setScanResult(result);
-      }
-  
-      function error(){
-        console.log("no code scanned");
-      }
-  },[])
-  
-  
+    });
 
+    scanner.render(success, error);
 
-  
+    function success(result) {
+      scanner.clear();
+      setScanResult(result);
+    }
+
+    function error() {
+      console.log("no code scanned");
+    }
+  }, []);
+
   return (
     <main className={styles.main}>
-      { scanResult ? (
-          // If the scan result was a success, do the following:
-          <div>
-            <p>Scan Result: {scanResult}</p>
-          </div>
-        ) : (
-          // Otherwise, do this:
-          <div id="reader"></div>
-        )
-      }
+      {scanResult ? (
+        // If the scan result was a success, do the following:
+        <div>
+          <p>Scan Result: {scanResult}</p>
+        </div>
+      ) : (
+        // Otherwise, do this:
+        <div id="reader"></div>
+      )}
 
-  <Podium titles={titles} />
+      <Podium titles={titles} />
+    </main>
   );
 }
