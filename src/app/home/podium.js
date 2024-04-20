c22620c25266f74dbe554b32ad399e0acaebd1a8
Mehul Tahiliani <mehul.tahiliani@gmail.com>
2024-04-20 01:47:50 -0400

    basic podium

--- src/app/home/podium.js
+++ src/app/home/podium.js
@@ -1,1 +1,21 @@
-export default function (props) {}
+import React from "react";
+import styles from "../page.module.css";
+
+export default function Podium(props) {
+  return (
+    <div className={styles.podium_container}>
+      <div className={styles.podium_item_container}>
+        <p className={styles.podium_item_text}>1</p>
+        <div className={styles.podium_item_fill}></div>
+      </div>
+      <div className={styles.podium_item_container}>
+        <p className={styles.podium_item_text}>2</p>
+        <div className={styles.podium_item_fill}></div>
+      </div>
+      <div className={styles.podium_item_container}>
+        <p className={styles.podium_item_text}>3</p>
+        <div className={styles.podium_item_fill}></div>
+      </div>
+    </div>
+  );
+}
