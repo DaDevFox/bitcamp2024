99cd746030e0ae9c6a4c41de1f1ae058875cdeb1
Justin Yang <111909590+JustinYang3@users.noreply.github.com>
2024-04-20 15:09:07 -0400

    Name and Points for placement

--- src/app/home/placement.js
+++ src/app/home/placement.js
@@ -4,7 +4,11 @@ export default function Placement(props) {
   return (
     <div className={styles.placement_container}>
       <div className={styles.leaderboard_item}>
-        <p className={styles.placement_text}>{props.buildingname}Ellicott</p>
+        <p></p>
+        <p className={styles.placement_building}>
+          {props.buildingname}Ellicott
+        </p>
+        <p className={styles.placement_points}>{props.position}5</p>
       </div>
     </div>
   );
