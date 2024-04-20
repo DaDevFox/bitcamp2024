e791c27b49106268a49e38426671b36720b1ef9f
Mehul Tahiliani <mehul.tahiliani@gmail.com>
2024-04-20 11:41:59 -0400

    fix merge conflicts

--- src/app/home/podium.js
+++ src/app/home/podium.js
@@ -7,21 +7,26 @@ import { Transition } from "react-transition-group";
 import { useState, useEffect } from "react";
 
 const defaultStyles = {
-  transition: `opacity 300ms ease-in-out`,
+  transition: `300ms ease-in-out`,
   opacity: 0,
-};
-
-const transitionStyles = {
-  entering: { opacity: 1 },
-  entered: { opacity: 1 },
-  exiting: { opacity: 0 },
-  exited: { opacity: 0 },
+  height: 0,
 };
 
 export default function Podium(props) {
   //const [heights, setHeights] = useState([]);
 
   const [showPodiums, setShowPodiums] = useState(false);
+
+  const masterTransitionStyles = [];
+
+  for (var i = 0; i < props.titles.length; i++) {
+    masterTransitionStyles[i] = {
+      entering: { height: props.points_normalized[i], opacity: 1 },
+      entered: { height: props.points_normalized[i], opacity: 1 },
+      exiting: { height: 0.1, opacity: 0 },
+      exited: { height: 0.1, opacity: 0 },
+    };
+  }
 
   useEffect(() => {
     setShowPodiums(!showPodiums);
@@ -38,7 +43,7 @@ export default function Podium(props) {
                 <div
                   style={{
                     ...defaultStyles,
-                    ...transitionStyles[state],
+                    ...masterTransitionStyles[index][state],
                   }}
                   className={styles.podium_item_fill}
                 ></div>
