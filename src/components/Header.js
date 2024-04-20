6caac40807a89a77e5e9435065e2b5038cb6ca82
Justin Yang <111909590+JustinYang3@users.noreply.github.com>
2024-04-20 11:33:30 -0400

    Temp header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
 import Link from "next/link";
+import { GrAchievement } from "react-icons/gr";
+import styles from "./header.module.css";
 
 export default function Header() {
-  <header>
-    <Link href="/">TerriPodium</Link>
-  </header>;
+  return (
+    <header>
+      <Link href="/">
+        {" "}
+        <GrAchievement className={styles.logo} />
+      </Link>
+      <Link href="/">
+        <b>TerraPodium</b>
+      </Link>
+      <Link href="/" className={styles.login}>
+        Log in
+      </Link>
+    </header>
+  );
 }
