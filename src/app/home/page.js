f58cf69ab24cbf5c6b153ab7a7bec41544dfa62b
SBhima <emailshivank@gmail.com>
2024-04-20 11:12:59 -0400

    Initial qr code changes

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,10 +1,53 @@
 import styles from "../page.module.css";
 
+"use client";
 import Podium from "./podium";
+import { Html5QrcodeScanner } from "html5-qrcode";
+import { useEffect, useState } from "react";
 
 export default function Home(props) {
+  
+  const [scanResult, setScanResult] = useState(null);
+  
+  useEffect(() => {
+    const scanner = new Html5QrcodeScanner('reader', {
+      qrbox: {
+        width: 250, // height and width of the scanning box
+        height: 250
+      },
+      fps: 5, // fps for qr code scanning, don't make it too high
+      })
+  
+      scanner.render(success, error);
+  
+      function success(result){
+        scanner.clear();
+        setScanResult(result);
+      }
+  
+      function error(){
+        console.log("no code scanned");
+      }
+  },[])
+  
+  
+
+
+  
   return (
     <main className={styles.main}>
+      { scanResult ? (
+          // If the scan result was a success, do the following:
+          <div>
+            <p>Scan Result: {scanResult}</p>
+          </div>
+        ) : (
+          // Otherwise, do this:
+          <div id="reader"></div>
+        )
+      }
+
+
       <Podium title1="Easton" title2="Denton" title3="Elicott" />
     </main>
   );
